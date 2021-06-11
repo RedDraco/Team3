168fb95737195e69ee5d833171c1087593d8d583
edison1000 <614edison@naver.com>
2021-06-11 22:43:02 +0900

    모아보기 기능 추가

--- app/src/main/java/com/example/team3/MainActivity.kt
+++ app/src/main/java/com/example/team3/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.team3
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -11,6 +12,7 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.example.team3.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import org.json.JSONException
 import org.json.JSONObject
 import java.text.DecimalFormat
@@ -18,18 +20,23 @@ import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-open class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var binding: ActivityMainBinding
+    lateinit var navigationView: NavigationView
+    var selectedMonth: String = ""
+    var selectedYear: String = ""
     private val url = "http://api.openweathermap.org/data/2.5/weather"
     private val appId = "b724ce4365af9091165c61e07361fb8e"
     var df = DecimalFormat("#.##")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.navigationView.itemIconTintList = null
         setContentView(binding.root)
-
+        navigationView = findViewById<NavigationView>(R.id.navigationView)
         init()
+        navigationView.setNavigationItemSelectedListener(this)
     }
 
     private fun init() {
@@ -44,10 +51,11 @@ open class MainActivity : AppCompatActivity() {
                 startActivity(settingIntent)
             }
             calendarView3.setOnDateChangeListener { view, year, month, dayOfMonth ->
+                selectedYear= year.toString()
                 calendarIntent.putExtra("year", year.toString())
 
-                val MONTH = if (month + 1 < 10) "0" + (month + 1) else (month + 1).toString()
-                calendarIntent.putExtra("month", MONTH)
+                selectedMonth = if (month + 1 < 10) "0" + (month + 1) else (month + 1).toString()
+                calendarIntent.putExtra("month", selectedMonth)
 
                 val DAY = if (dayOfMonth < 10) "0$dayOfMonth" else dayOfMonth.toString()
                 calendarIntent.putExtra("day", DAY)
@@ -56,10 +64,10 @@ open class MainActivity : AppCompatActivity() {
 
             }
             getWeatherBtn.setOnClickListener {
-                val city = editCity!!.text.toString().trim { it <= ' '}
-                if(city==""){
+                val city = editCity!!.text.toString().trim { it <= ' ' }
+                if (city == "") {
                     Toast.makeText(applicationContext, "도시를 입력해주세요", Toast.LENGTH_SHORT).show()
-                }else{
+                } else {
                     var tempUrl = "$url?q=$city&appid=$appId"
                     val stringRequest =
                         StringRequest(Request.Method.POST, tempUrl,
@@ -78,7 +86,8 @@ open class MainActivity : AppCompatActivity() {
                                     weatherText.text = description
                                     tempText.text = df.format(temp) + "°C"
                                     cityNameText.text = cityName
-                                    Glide.with(applicationContext).load(iconUrl).into(weatherImageView)
+                                    Glide.with(applicationContext).load(iconUrl)
+                                        .into(weatherImageView)
 
                                 } catch (e: JSONException) {
                                     e.printStackTrace()
@@ -94,8 +103,42 @@ open class MainActivity : AppCompatActivity() {
                     requestQueue.add(stringRequest)
                 }
             }
-            }
         }
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val showAllIntent = Intent(this, ShowAllActivity::class.java)
+
+        binding.apply {
+            when(item.itemId){
+                R.id.nav_explain -> {
+                    if(selectedYear == "" && selectedMonth == ""){
+                        var calendar: Calendar = Calendar.getInstance()
+                        var dateFormat = SimpleDateFormat("yyyy-MM")
+                        var date = dateFormat.format(calendar.time)
+                        showAllIntent.putExtra("date", date)
+                        startActivity(showAllIntent)
+                    }else{
+                        var date = "$selectedYear-$selectedMonth"
+                        showAllIntent.putExtra("date", date)
+                        startActivity(showAllIntent)
+                    }
+                }
+            }
+        }
+        return false
+    }
+
+    override fun onBackPressed() {
+        binding.apply {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawers()
+            } else {
+                super.onBackPressed()
+            }
+        }
+
+    }
+}
 
 
